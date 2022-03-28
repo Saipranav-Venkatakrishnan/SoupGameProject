4993eaf60c4b5486428bbc971c42374832383ab9
Saipranav-Venkatakrishnan <saipranavvk@gmail.com>
2022-03-28 10:53:09 -0500

    Added SettingsPage

---
+++ app/src/main/java/com/example/soupgameproject/SettingsPage.java
@@ -0,0 +1,13 @@
+package com.example.soupgameproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_page);
+    }
+}
