a050f4b1844aa70a95795f282833b233621b351f
nsaunders113 <nicksaunders113@gmail.com>
2022-04-22 10:36:03 -0500

    catalog ugly layout

---
+++ app/src/main/java/com/example/soupgameproject/CatalogPage.java
@@ -0,0 +1,15 @@
+package com.example.soupgameproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatalogPage extends AppCompatActivity {
+
+    // https://stackoverflow.com/questions/63661601/add-many-cardviews-with-onclick-method-programmatically
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog_page);
+    }
+}
